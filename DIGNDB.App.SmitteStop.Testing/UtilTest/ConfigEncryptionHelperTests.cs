7b2c8cf418540c993502372d87edd69616858d1f
Mateusz Tracz <mtra@netcompany.com>
2020-11-03 14:43:28 +0100

    Remove Windows DataProtection API usages and adjust unit tests for it

--- DIGNDB.App.SmitteStop.Testing/UtilTest/ConfigEncryptionHelperTests.cs
+++ DIGNDB.App.SmitteStop.Testing/UtilTest/ConfigEncryptionHelperTests.cs
@@ -1,42 +1,29 @@
 ﻿using NUnit.Framework;
 using DIGNDB.App.SmitteStop.Core.Helpers;
-using System.Security.Cryptography;
-using System.Text;
 using System;
+using FluentAssertions;
 
 namespace DIGNDB.App.SmitteStop.Testing.UtilTest
 {
     [TestFixture]
     public class ConfigEncryptionHelperTests
     {
-        private byte[] s_aditionalEntropy = { 2, 1, 6, 8, 11 };
-        
-        [Test]
         [TestCase("testEncrypt")]
         [TestCase("abc123@[.]")]
         [TestCase("===@Nghjt")]
         public void ConfigEncryptionHelper_UnprotectedData_ShouldReturnString(string expectValue)
         {
-            var encryptedExpectValue = ProtectedData.Protect(Encoding.UTF8.GetBytes(expectValue), 
-                s_aditionalEntropy, DataProtectionScope.CurrentUser);
-            var encryptedExpectValueToBase64 = Convert.ToBase64String(encryptedExpectValue);
-            var decryptedValue = ConfigEncryptionHelper.UnprotectString(encryptedExpectValueToBase64);
-            Assert.AreEqual(expectValue, decryptedValue);
+            Action unprotectString = () => ConfigEncryptionHelper.UnprotectString(null);
+            unprotectString.Should().Throw<NotImplementedException>();
         }
 
-        [Test]
         [TestCase("testEncrypt")]
         [TestCase("abc123@[.]")]
         [TestCase("===@Nghjt")]
         public void ConfigEncryptionHelper_ProtectedData_ShouldReturnSameDataWhenDecrypted(string data)
         {
-            var encryptedData = ConfigEncryptionHelper.ProtectString(data);
-            
-            //decrypteding to check
-            var unprotectData = ProtectedData.Unprotect(Convert.FromBase64String(encryptedData),
-                s_aditionalEntropy, DataProtectionScope.CurrentUser);
-            var expectData = Encoding.UTF8.GetString(unprotectData);
-            Assert.AreEqual(expectData, data);
+            Action protectString = () => ConfigEncryptionHelper.ProtectString(data);
+            protectString.Should().Throw<NotImplementedException>();
         }
     }
 }
