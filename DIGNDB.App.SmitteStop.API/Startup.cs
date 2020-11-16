d8b5784c5c44ca2ae676e3679753bca5b808ef65
Mateusz Tracz <mtra@netcompany.com>
2020-11-16 13:49:58 +0100

    Use new registration methods in Startups

--- DIGNDB.App.SmitteStop.API/Startup.cs
+++ DIGNDB.App.SmitteStop.API/Startup.cs
@@ -52,115 +52,12 @@ namespace DIGNDB.App.SmitteStop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers().AddControllersAsServices();
-            // configure jwt authentication
-            services.AddAuthorization();
-            services.AddMvc(options =>
-            {
-                options.Filters.Add(typeof(DeprecatedCheckAttribute));
-            }).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            // Add API versioning to as service to your project
-            services.AddApiVersioning(config =>
-            {
-                // Specify the default API Version
-                config.DefaultApiVersion = new ApiVersion(3, 0);
-                // If the client hasn't specified the API version in the request, use the default API version number
-                config.AssumeDefaultVersionWhenUnspecified = true;
-                // Advertise the API versions supported for the particular endpoint
-                config.ReportApiVersions = true;
-                config.ApiVersionReader = ApiVersionReader.Combine(
-                        new UrlSegmentApiVersionReader()
-                );
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API version 1", Version = "v1" });
-                c.SwaggerDoc("v2", new OpenApiInfo { Title = "API version 2", Version = "v2" });
-                c.SwaggerDoc("v3", new OpenApiInfo { Title = "API version 3", Version = "v3" });
-                c.OperationFilter<RemoveVersionParameterAttribute>();
-                c.DocumentFilter<FilterRoutesDocumentFilter>();
-                c.DocumentFilter<ReplaceVersionWithExactValueInPathAttribute>();
-                c.EnableAnnotations();
-                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.XML";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
-            var appsettingsConfig = Configuration.GetSection("AppSettings").Get<AppSettingsConfig>();
-            ModelValidator.ValidateContract(appsettingsConfig);
-
-            var logValidationRulesConfig = Configuration.GetSection("LogValidationRules").Get<LogValidationRulesConfig>();
-            ModelValidator.ValidateContract(logValidationRulesConfig);
-
-            var keyValidationRulesConfig = Configuration.GetSection("KeyValidationRules").Get<KeyValidationConfiguration>();
-            ModelValidator.ValidateContract(logValidationRulesConfig);
-
-            services.AddSingleton(appsettingsConfig);
-            services.AddSingleton<IOriginSpecificSettings>(appsettingsConfig);
-            services.AddSingleton<IPackageBuilderConfig>(appsettingsConfig);
-            
-            services.AddSingleton(logValidationRulesConfig);
-            services.AddSingleton(keyValidationRulesConfig);
-
-            var jwtAuthorizationConfig = Configuration.GetSection(nameof(JwtAuthorization)).Get<JwtAuthorization>();
-            ModelValidator.ValidateContract(jwtAuthorizationConfig);
-            services.AddSingleton(jwtAuthorizationConfig);
-
-            services.AddAutoMapper(typeof(CountryMapper));
-            services.AddSingleton(new AuthOptions(_env.IsDevelopment()));
-            services.AddSingleton(Configuration);
-            services.AddScoped<MobileAuthorizationAttribute>();
-            services.AddScoped<DeprecatedCheckAttribute>();
-            services.AddScoped<AuthorizationAttribute>();
-
-            services.AddHsts(options =>
-            {
-                options.MaxAge = DateTime.Now.AddYears(1) - DateTime.Now;
-                options.IncludeSubDomains = true;
-            });
-
-            var connectionString = Configuration["SQLConnectionString"];
-            services.AddScoped<ICacheOperations, CacheOperations>();
-            services.AddScoped<IDatabaseKeysToBinaryStreamMapperService, DatabaseKeysToBinaryStreamMapperService>();
-            services.AddScoped(typeof(ITemporaryExposureKeyRepository), typeof(TemporaryExposureKeyRepository));
-            services.AddScoped<IExposureKeyMapper, ExposureKeyMapper>();
-            services.AddScoped<IExposureKeyValidator, ExposureKeyValidator>();
-            services.AddScoped<ILogMessageValidator, LogMessageValidator>();
-            services.AddScoped<IAppleService, AppleService>();
-            services.AddScoped<IKeysListToMemoryStreamConverter, KeysListToMemoryStreamConverter>();
-            services.AddScoped<ICountryService, CountryService>();
-            services.AddScoped<IPackageBuilderService, PackageBuilderService>();
-            services.AddScoped<IAddTemporaryExposureKeyService, AddTemporaryExposureKeyService>();
-            services.AddScoped<IZipFileInfoService, ZipFileInfoService>();
-            services.AddScoped<IFileSystem, FileSystem>();
-
-            services.AddDbContext<DigNDB_SmittestopContext>(opts =>
-                opts.UseSqlServer(connectionString, x => x.MigrationsAssembly("DIGNDB.App.SmitteStop.DAL")));
-            services.AddScoped<DigNDB_SmittestopContext>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            services.AddScoped(typeof(IDataAccessLoggingService<>), typeof(DataAccessLoggingService<>));
-            services.AddScoped<ICountryRepository, CountryRepository>();
-
-
-            services.AddSingleton<IExposureConfigurationService, ExposureConfigurationService>();
-            services.AddSingleton<IExportKeyConfigurationService, ExportKeyConfigurationService>();
-
-            services.AddScoped<IKeyValidator, KeyValidator>();
-            services.AddScoped<IEpochConverter, EpochConverter>();
-            services.AddScoped<IRiskCalculator, RiskCalculator>();
+            services.AddAPIDependencies(Configuration, _env);
 
             services.AddCoreDependencies();
             services.AddDALDependencies();
 
-            services.AddMemoryCache();
-
-            var controllers = Assembly.GetExecutingAssembly().GetTypes()
-                .Where(type => typeof(ControllerBase).IsAssignableFrom(type))
-                .ToList();
-            InjectionChecker.CheckIfAreAnyDependenciesAreMissing(services, controllers);
+            InjectionChecker.CheckIfAreAnyDependenciesAreMissing(services, Assembly.GetExecutingAssembly(), typeof(ControllerBase));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
