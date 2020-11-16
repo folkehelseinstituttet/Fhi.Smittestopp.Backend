2e5eca91c6f37d0628a36380e679ed3098a2276a
Mateusz Tracz <mtra@netcompany.com>
2020-11-16 14:13:12 +0100

    Add migration for adding Norway to Country table

---
+++ DIGNDB.App.SmitteStop.DAL/Migrations/20201116131119_AddNorwayToCountryTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DIGNDB.App.SmitteStop.DAL.Migrations
+{
+    public partial class AddNorwayToCountryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Country",
+                columns: new[] { "Id", "Code", "PullingFromGatewayEnabled", "VisitedCountriesEnabled" },
+                values: new object[] { 29L, "NO", false, false });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 29L);
+        }
+    }
+}
