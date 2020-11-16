b30ea1bdc95916a10a9d2d64ece0c0d103b932a3
Mateusz Tracz <mtra@netcompany.com>
2020-11-16 13:49:13 +0100

    Add registration for FederationGatewayApi module

---
+++ FederationGatewayApi/ContainerRegistration.cs
@@ -0,0 +1,23 @@
+﻿using DIGNDB.App.SmitteStop.Core.Contracts;
+using FederationGatewayApi.Contracts;
+using FederationGatewayApi.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FederationGatewayApi
+{
+    public static class ContainerRegistration
+    {
+        public static IServiceCollection AddGatewayDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IKeyFilter, KeyFilter>();
+            services.AddScoped<IEuGatewayService, EuGatewayService>();
+            services.AddScoped<IGatewayWebContextReader, GatewayWebContextReader>();
+            services.AddScoped<IGatewayHttpClient, GatewayHttpClient>();
+            services.AddScoped<IEFGSKeyStoreService, EFGSKeyStoreService>();
+            services.AddScoped<IDaysSinceOnsetOfSymptomsDecoder, DaysSinceOnsetOfSymptomsDecoder>();
+            services.AddScoped<ISignatureService, SignatureService>();
+
+            return services;
+        }
+    }
+}
