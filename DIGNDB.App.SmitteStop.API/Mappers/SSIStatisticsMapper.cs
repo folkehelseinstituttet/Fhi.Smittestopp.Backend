cf355f43773b2156bdca6b9b5e3e3f39772cc757
Bartosz Polak <bapo@netcompany.com>
2021-03-03 11:09:52 +0100

    Revert "Revert "Added db model and controller for covid statistics""
    This reverts commit 25672952e740ec57e78ce39c722cf037b56a78b8.

---
+++ DIGNDB.App.SmitteStop.API/Mappers/SSIStatisticsMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DIGNDB.App.SmitteStop.Domain.Db;
+using DIGNDB.App.SmitteStop.Domain.Dto;
+
+namespace DIGNDB.App.SmitteStop.API.Mappers
+{
+    public class SSIStatisticsMapper : Profile
+    {
+        public SSIStatisticsMapper()
+        {
+            CreateMap<SSIStatistics, SSIStatisticsDto>()
+                .ForMember(x => x.Date,
+                    opts => opts.MapFrom(x => x.Date));
+        }
+    }
+}
---
+++ DIGNDB.App.SmitteStop.Domain/Db/ApplicationStatistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DIGNDB.App.SmitteStop.Domain.Db
+{
+    public class ApplicationStatistics
+    {
+        public int Id { get; set; }
+        public int PositiveResultsLast7Days { get; set; }
+        public int TotalSmittestopDownloads { get; set; }
+        public int PositiveTestsResultsTotal { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
