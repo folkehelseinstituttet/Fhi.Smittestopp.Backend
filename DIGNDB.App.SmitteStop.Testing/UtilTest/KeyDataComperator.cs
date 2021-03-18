71bdfa1323f7652a4403f1fad0ac8440919ad14c
Bartosz Polak <bapo@netcompany.com>
2021-03-18 13:45:48 +0100

    fixed gateway download test

---
+++ DIGNDB.App.SmitteStop.Testing/UtilTest/KeyDataComperator.cs
@@ -0,0 +1,12 @@
+﻿using Google.Protobuf;
+using System.Collections;
+
+namespace DIGNDB.App.SmitteStop.Testing.UtilTest
+{
+    public static class KeyDataComperator
+    {
+        public static bool EqualsKeyData(ByteString protoKeyData, ByteString keyDataToCompare) => EqualsKeyData(protoKeyData.ToByteArray(), keyDataToCompare.ToByteArray());
+
+        public static bool EqualsKeyData(byte[] keyData1, byte[] keyData2) => StructuralComparisons.StructuralEqualityComparer.Equals(keyData1, keyData2);
+    }
+}
